eb40173d8b82a28dd1b2260ed5343fafad6d6eec
Vikas99Rajak <84638696+Vikas99Rajak@users.noreply.github.com>
2021-05-23 14:31:52 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,25 @@
+document.write("Hello;");
+document.write(" To be or not to be ;")
+var phrase = "To be or not to be";
+var text = "Vikas"
+document.write("length of the phrase = ")
+document.write(phrase.length);
+document.write("; It counts even space as a character ");
+document.write("; Convert the phrase to uppercase = ")
+document.write(phrase.toUpperCase());
+document.write("; Convert the phrase to lowercase = ")
+document.write(phrase.toLowerCase());
+document.write("; To find the character at index 1 = ")
+document.write(phrase.charAt(1));
+document.write("; To find the character at index 8 = ")
+document.write(phrase.charAt(8));
+document.write("; To find the character at index 9 = ")
+document.write(phrase.charAt(9));
+document.write("; To find the index of r = ")
+document.write(phrase.indexOf("r"));
+document.write("; To find the index of blank space = ")
+document.write(phrase.indexOf(" "));
+document.write("; Appending of phrase and text = ")
+document.write(phrase+text);
+document.write("; Appending of phrase and text = ")
+document.write(phrase+" "+text);
